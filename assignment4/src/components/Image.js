ed262a199b3ad983a6104d41005674350c914cb0
Savindu_Rashmika <savindurashmika2000@gmail.com>
2024-11-23 20:25:09 +0530

    create the game complete

--- assignment4/src/components/Image.js
+++
@@ -1,5 +0,0 @@
- export default function Image(){
-    return(
-        <h1>dd</h1>
-    );
-}
--- assignment4/src/components/Main.js
+++ assignment4/src/components/Main.js
@@ -1,23 +1,65 @@
-import {useState} from 'react';
-import {animals} from '../data/AnimalsDb'
+import { useState } from 'react';
+import { animals } from '../data/AnimalsDb';
 
-export default function Main(){
-    return(
-       <table width='100%'allign-item='center'>
-        <tr> 
-            <td  colspan='4'>ss</td>
-        </tr>
-        <tr>
-            <td>A</td>
-            <td>B</td>
-            <td colspan='2'>
-                {
-                   animals.map((index,animal)=>(
-                   <h1 key={index}>{animal.name}</h1>
-                   ))
-                }    
-            </td>
-        </tr>
-       </table>
+export default function Main() {
+   
+    const randomname = () => {
+        const index = Math.floor(Math.random() * animals.length);
+        return animals[index];
+    };
+
+
+    const [target, setTarget] = useState(randomname);
+    const [result, setResult] = useState('null');
+
+    const checker = (animal) => {
+        if (animal === target) {
+            setResult('Win');
+        } else {
+            setResult('Lost');
+        }
+    };
+
+    return (
+        <table width="100%" style={{ textAlign: 'center' }}>
+            <thead>
+                <tr>
+                    <th colSpan="4">Animal Game</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td width="20%">Result</td>
+                    <td width="20%">Animal Name</td>
+                    <td colSpan="2">Select Animal</td>
+                </tr>
+                <tr>
+                    <td width="20%">{result}</td>
+                    <td width="20%">{target.name}</td>
+                    <td colSpan="2">
+                        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px' }}>
+                            {animals.map((animal, index) => (
+                                <div
+                                    key={index}
+                                    onClick={() => checker(animal)}
+                                   
+                                >
+                                    <img
+                                        src={require(`../assets/fig/${animal.img}`)}
+                                        alt={animal.name}
+                                        style={{
+                                            width: '150px',
+                                            height: '150px',
+                                            objectFit: 'cover',
+                                            borderRadius: '5px',
+                                        }}
+                                    />
+                                </div>
+                            ))}
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     );
 }
